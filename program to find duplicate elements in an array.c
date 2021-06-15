388b44c38bcf855be6794af082f86eb3e8d9c35e
samriddhi110 <85922361+samriddhi110@users.noreply.github.com>
2021-06-15 17:46:48 +0530

    Add files via upload

---
+++ program to find duplicate elements in an array.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int a = 0 , b = 1 , c , i , n;
+    printf("define the limit of the fibocanni series");
+    scanf("%d" , &n);
+    
+    c = a + b;
+    
+    for(i=1, i==n , i++ , a++ , b++);
+    {
+        printf("%d" ,c);
+    }
+
+    return 0;
+}
