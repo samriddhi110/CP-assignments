9d7552a6ea8506ab5148bdfc541fa1e2bdabcf89
samriddhi110 <85922361+samriddhi110@users.noreply.github.com>
2021-06-15 18:28:36 +0530

    Add files via upload

---
+++ program to find the largest element in an array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int findmaxelement(int a[10])
+{   int max = a[0],i;
+ 
+    for (i = 1; i < 10; i++)
+        if (a[i] > max)
+            max = a[i];
+    return max;
+}
+ 
+int main()
+{
+    int a[10],i;
+    printf("enter the values");
+    for (i = 1; i < 10; i++)
+    scanf("%d",&a[i]);
+    
+    printf("Largest in given array is %d", findmaxelement(a));
+    return 0;
+}
